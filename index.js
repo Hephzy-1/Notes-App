25e2e1970141d158f5cd09a19a39a0a7116998b8
Hephzy-1 <hzdelight01@gmail.com>
2024-02-05 17:41:07 +0100

    I'm done with this

--- index.js
+++ index.js
@@ -2,17 +2,43 @@ const notesCon = document.querySelector('.notes');
 const createBtn = document.querySelector('.btn');
 let notes = document.querySelectorAll('.input-box');
 
-createBtn.addEventListener('click', ()=> {
+function showNotes() {
+  notesCon.innerHTML = localStorage.getItem("notes")
+}
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesCon.innerHTML);
+}
+
+createBtn.addEventListener('click', () => {
   let inputBox = document.createElement('p');
   let img = document.createElement('img');
-  inputBox.className = 'input-box';
-  inputBox.setAttribute('contentditable', 'true');
+  inputBox.className = 'input-box'; 
+  inputBox.setAttribute('contenteditable', 'true');
   img.src = 'img/delete-removebg-preview.png';
   notesCon.appendChild(inputBox).appendChild(img);
 })
 
+
 notesCon.addEventListener('click', function(e){
   if (e.target.tagName === 'IMG') {
     e.target.parentElement.remove();
+    updateStorage();
+  }
+  else if (e.target.tagName === "p") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach(nt => {
+      nt.onkeyup = function() {
+        updateStorage();
+      }
+    })
+  }  
+})
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    document.execCommand('insertLineBreak');
+    event.preventDefault();
   }
 })
